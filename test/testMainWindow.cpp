be09d9c8b9390d0837026029849cc1d22d43967e
Andreas Holzner <andreas@dieholzners.de>
2021-02-12 01:51:19 +0100

    Add trompeloeil as mocking framework

--- test/testMainWindow.cpp
+++ test/testMainWindow.cpp
@@ -1,24 +1,36 @@
-#include <catch2/catch.hpp>
-#include <memory>
-
-#include <QtTest>
 #include <QAction>
+#include <QTabWidget>
+#include <QtTest>
+#include <catch2/catch.hpp>
+#include <catch2/trompeloeil.hpp>
+#include <memory>
+#include <vector>
 
+#include "BackupConfig.h"
 #include "BorgmaticManager.h"
 #include "MainWindow.h"
-#include <QTabWidget>
+
+class BorgmaticManagerMock : public trompeloeil::mock_interface<BorgmaticManager> {
+  IMPLEMENT_MOCK0(newBorgmaticConfig);
+  IMPLEMENT_MOCK1(removeConfig);
+  IMPLEMENT_MOCK0(loadSettings);
+  IMPLEMENT_MOCK0(saveSettings);
+  IMPLEMENT_MOCK0(configs);
+};
 
 TEST_CASE("MainWindow", "[ui]") {
-  auto uniqueManager = std::make_unique<BorgmaticManager>();
+  auto uniqueManager = std::make_unique<BorgmaticManagerMock>();
   auto manager = uniqueManager.get();
+  ALLOW_CALL(*manager, configs()).RETURN(std::vector<std::shared_ptr<BackupConfig>>());
   auto mainWindow = MainWindow{std::move(uniqueManager)};
   auto tabWidget = mainWindow.findChild<QTabWidget *>("borgmaticTabWidget");
   REQUIRE(tabWidget->count() == 0);
 
   SECTION("New Config menu entry adds a new tab") {
-    mainWindow.findChild<QAction*>("menuNew")->trigger();
+    REQUIRE_CALL(*manager, newBorgmaticConfig()).RETURN(std::make_shared<BackupConfigImpl>());
 
-    REQUIRE(manager->configs.size() == 1);
+    mainWindow.findChild<QAction *>("menuNew")->trigger();
+
     REQUIRE(tabWidget->count() == 1);
   }
 }
