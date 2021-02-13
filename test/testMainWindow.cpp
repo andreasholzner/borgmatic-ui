ac77a88700cc071cbdbbac37fb1867ffae04741f
Andreas Holzner <andreas@dieholzners.de>
2021-02-13 22:51:57 +0100

    Complete tests for MainWindow

--- test/testMainWindow.cpp
+++ test/testMainWindow.cpp
@@ -18,19 +18,42 @@ class BorgmaticManagerMock : public trompeloeil::mock_interface<BorgmaticManager
   IMPLEMENT_MOCK0(configs);
 };
 
+std::vector<std::shared_ptr<BackupConfig>> prepareConfigs(std::vector<std::string> const &configNames) {
+  std::vector<std::shared_ptr<BackupConfig>> res{configNames.size()};
+  std::transform(configNames.begin(), configNames.end(), res.begin(), [](std::string const &configName) {
+    auto backupConfig = std::make_shared<BackupConfigImpl>();
+    backupConfig->borgmaticConfigFile(configName);
+    return std::static_pointer_cast<BackupConfig>(backupConfig);
+  });
+  return res;
+}
+
 TEST_CASE("MainWindow", "[ui]") {
   auto uniqueManager = std::make_unique<BorgmaticManagerMock>();
   auto manager = uniqueManager.get();
-  ALLOW_CALL(*manager, configs()).RETURN(std::vector<std::shared_ptr<BackupConfig>>());
-  auto mainWindow = MainWindow{std::move(uniqueManager)};
-  auto tabWidget = mainWindow.findChild<QTabWidget *>("borgmaticTabWidget");
-  REQUIRE(tabWidget->count() == 0);
+
+  SECTION("MainWindow creates a tab for every config entry upon creation") {
+    REQUIRE_CALL(*manager, configs()).RETURN(prepareConfigs({"name1", "name2"}));
+
+    auto mainWindow = MainWindow{std::move(uniqueManager)};
+
+    auto tabWidget = mainWindow.findChild<QTabWidget *>("borgmaticTabWidget");
+    REQUIRE(tabWidget->count() == 2);
+    REQUIRE(tabWidget->tabText(0) == QString{"name1"});
+    REQUIRE(tabWidget->tabText(1) == QString{"name2"});
+  }
 
   SECTION("New Config menu entry adds a new tab") {
+    ALLOW_CALL(*manager, configs()).RETURN(prepareConfigs({}));
+    auto mainWindow = MainWindow{std::move(uniqueManager)};
+    auto tabWidget = mainWindow.findChild<QTabWidget *>("borgmaticTabWidget");
+    REQUIRE(tabWidget->count() == 0);
+
     REQUIRE_CALL(*manager, newBorgmaticConfig()).RETURN(std::make_shared<BackupConfigImpl>());
 
     mainWindow.findChild<QAction *>("menuNew")->trigger();
 
     REQUIRE(tabWidget->count() == 1);
+    REQUIRE(tabWidget->tabText(0) == QString{});
   }
 }
